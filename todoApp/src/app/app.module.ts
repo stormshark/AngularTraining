83db574287991daeeac988415dbb6f3a1145ef0b
stormshark <53909021+stormshark@users.noreply.github.com>
2024-12-23 23:27:52 +0300

    aa

--- todoApp/src/app/app.module.ts
+++ todoApp/src/app/app.module.ts
@@ -4,13 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
 })
-export class AppModule { }
+export class AppModule {}
